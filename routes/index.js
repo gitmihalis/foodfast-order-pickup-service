23fea0528764d5975881ae4c8cc22ec370965dfe
lpenstone <laura.penstone@queensu.ca>
2017-07-20 18:59:43 +0000

    Working shopping cart with add and remove buttons functioning

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/test', function(req, res){
+  if (!req.body['quantities[]'][1]){
+    let name = req.body['names[]'];
+    let quantity = parseInt(req.body['quantities[]']);
+    console.log(name, quantity);
+  } else{
+    for (let value in req.body['quantities[]']){
+      let name = req.body['names[]'][value];
+      let quantity = parseInt(req.body['quantities[]'][value]);
+      console.log(name, quantity);
+    }
+  }
+  let payMethod = req.body.payMethod;
+  let customer = req.body.customerName;
+  console.log(payMethod, customer)
+});
+
+
+
 module.exports = router;

dcbc314abce7743b71555d2d6a838ac5a0a1a82c
gitmihalis <mihalisfthenos@icloud.com>
2017-07-20 19:54:46 +0000

    fix bad route for twilio

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var twilio = require('./routes/twilio');
-// var users = require('./routes/users');
+var orders = require('./routes/orders');
 
 var app = express();
 
@@ -24,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/twilio', twilio);
+app.use('/orders', orders);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

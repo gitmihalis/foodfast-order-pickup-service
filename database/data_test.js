6dc5006276141b35f0600af0b1a0e1be4f74c78c
egomatsushita <eduardo.go.rj@gmail.com>
2017-07-21 04:47:18 +0000

    Refactor database retrieve test file

--- database/data_test.js
+++ database/data_test.js
@@ -1,17 +1,18 @@
 const settings = require("./settings");
 const knex = require('knex') (require('./knexfile').development);
 
-const name = "Richard";
-const phone = "14164523434";
+const name = "Laura";
+const phone = "14164526666";
 const items = [ "BMT", "Special", "Colorful"];
-const quantities = [ 4, 5, 6];
+const quantities = [ 3, 3, 3];
 
 const promises = () => {
+  // retrieve all of records of items' table
   return knex.select('*')
     .from('items')
     .then((items_object) => {
       return items_object;
-    })
+    });
 }
 
 promises()
@@ -21,104 +22,44 @@ promises()
     let item_prices = [];
 
     items.map((aItem) => {
+      let i = 0;
       for (let item of Object.keys(items_object)) {
+        // push to item_ids and item_prices when match the item.name
         if (aItem == items_object[item]['name']) {
           const _item = items_object[item];
-
           item_ids.push(_item.id);
           item_prices.push(_item.item_price);
-          total += +(_item.item_price);
+          total += +(_item.item_price * quantities[i]);
         }
+        i++;
       }
     });
 
-    // knex('orders')
-    //   .insert([{
-    //     'cost': total,
-    //     'name': name,
-    //     'phone_number': phone,
-    //     'status': 'concluded'
-    //   }])
-    //   .asCallback((err, rows) => {
-    //     if (err) {
-    //       return console.error(err);
-    //     }
-    //   });
-
-
-      const promises1 = () => {
-        return new Promise((resolve, reject) => {
-          knex.select('id')
-            .from('orders')
-            .orderBy('id', 'desc')
-            .limit(1)
-            .then((rows) => {
-              return resolve(rows[0].id);
-            });
-        })
-      }
-
-      const promises2 = (i) => {
-        return promises1()
-          .then((result) => {
-            knex('orderitems')
-              .insert([{
-                'order_id': result,
-                'item_id': item_ids[i],
-                'quantity': quantities[i],
-                'paid_each': item_prices[i]
-              }])
-              .asCallback((err, rows) => {
-                if (err) {
-                  return console.error(err);
-                }
-              });
-          })
-          .catch((err) => {
-            console.error('error: ', err);
-          })
-      }
-
-
-      let promises_array = [];
-
-      for (let item of items) {
+    // create a new record in orders table
+    knex('orders')
+      .insert([{
+        'cost': total,
+        'name': name,
+        'phone_number': phone,
+        'status': 'concluded'
+      }])
+      .returning('id')
+      .then(function (id) {
+        for (let item of items) {
           let i = items.indexOf(item);
-        promises_array.push(promises2(i));
-      }
-let j = 0;
-      Promise.all(promises_array).then((value) => {
-        console.log('j', j);
-        console.log(value);
-        j++;
-      })
-
-
-
-
-    // for (let i = 0; i < items.length; i++) {
-
-
-
-    //   promises1()
-    //     .then((result) => {
-    //       knex('orderitems')
-    //         .insert([{
-    //           'order_id': result,
-    //           'item_id': item_ids[i],
-    //           'quantity': quantities[i],
-    //           'paid_each': item_prices[i]
-    //         }])
-    //         .asCallback((err, rows) => {
-    //           if (err) {
-    //             return console.error(err);
-    //           }
-    //         });
-    //     })
-    //     .catch((err) => {
-    //       console.error('error: ', err);
-    //     })
-    // }
-    console.log('finish...')
-  })
-
+          // create a new record of each item of the order
+          knex('orderitems')
+            .insert([{
+              'order_id': JSON.parse(id),
+              'item_id': item_ids[i],
+              'quantity': quantities[i],
+              'paid_each': item_prices[i]
+            }])
+            .asCallback((err, rows) => {
+              if (err) {
+                return console.error(err);
+              }
+            });
+        }
+      });
+  });

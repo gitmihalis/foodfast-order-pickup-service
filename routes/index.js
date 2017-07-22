0ff92888561996293b6e084c9731fa2b66d96d8e
lpenstone <laura.penstone@queensu.ca>
2017-07-22 16:59:05 +0000

    changing routes for login and register

--- routes/index.js
+++ routes/index.js
@@ -59,57 +59,7 @@ router.get("/manager", (req, res) => {
   res.render("manager");
 });
 
-router.get("/login", (req, res) => {
-  //let templateVars = { user: users[req.session.user_id] };
-  res.status(200);
-  res.render("login");
-});
 
-router.get("/register", (req, res) => {
-  //let templateVars = { user: users[req.session.user_id] };
-  res.status(200);
-  res.render("register");
-});
-
-router.post('/login', (req, res) => {
-  User.authenticate(req.body.email, req.body.password)
-  .then((user) => {
-    // If email and password match, we assign the id to the session
-    req.session.user_id = user.id;
-    res.redirect('/');
-  }).catch((err) => {
-    // In the event that an error occurred at any point during the promise
-    // chain, add the error message to the flash errors and redirect.
-    req.flash('errors', err.message);
-    res.redirect('/manager');
-  });
-});
-
-
-router.post('/register', (req, res) => {
-  if (!req.body.email || !req.body.password) {
-    // If the registration form was submitted without a value for email or
-    // password, then set an error message and redirect.
-    req.flash('errors', 'email and password are required');
-    res.redirect('/');
-    // IMPORTANT: always return after sending a response, whether it's a
-    // redirect, render, send, end, json, or whatever.
-    return;
-  }
-  User.add(req.body.email, req.body.password)
-  .then(() => {
-    // This callback will be called after the promise returned by the last
-    // call to .then has resolved. That happens after the user is inserted
-    // into the database.
-    req.flash('info', 'account successfully created');
-    res.redirect('/');
-  }).catch((err) => {
-    // In the even that an error occurred at any point during the promise
-    // chain, add the error message to the flash errors and redirect.
-    req.flash('errors', err.message);
-    res.redirect('/');
-  });
-});
 
 
 

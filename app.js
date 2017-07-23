75803525decf0fb85ed486500cc73571f609135f
egomatsushita <eduardo.go.rj@gmail.com>
2017-07-23 15:22:12 +0000

    Require cookie session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
 
 //const knex = require('knex') (require('./database/knexfile').development);
@@ -28,6 +29,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieSession( {
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
 
 app.use('/', index);
 app.use('/orders', orders);

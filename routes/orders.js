669ae72c382c472d788a30b60e0d8b37387f66b2
gitmihalis <mihalisfthenos@icloud.com>
2017-07-23 21:18:37 +0000

    persist order id

--- routes/orders.js
+++ routes/orders.js
@@ -7,17 +7,8 @@ const Order = require('../lib/order')(knex);
 const Item = require('../lib/item')(knex);
 const OrderItem = require('../lib/order_items')(knex);
 
-const basicAuth = require('express-basic-auth');
-
-const staticUserAuth = basicAuth({
-    users: {
-        'Admin': 'secret1234'
-    },
-    challenge: true
-})
-
 // -------- ROUTES ------------
-router.get('/', staticUserAuth, (req, res) => {
+router.get('/' ,(req, res) => {
 
   res.status(200);
   res.render("manager", {title: 'Orders list'});
@@ -27,21 +18,23 @@ router.get('/', staticUserAuth, (req, res) => {
 router.post('/', (req, res) => {
 	// * 
 	console.log('request body is : ', req.body);
-	const itemQuants = req.body['quantities[]'];
-	const itemNames = req.body['names[]'];
+	let itemQuants = req.body['quantities[]'];
+	let itemNames = req.body['names[]'];
+	let orderID  = null;
+	console.log('itemnames is a ', typeof itemNames)
+	itemNames = ( typeof itemNames === 'string' ) ? [itemNames] : itemNames;
+	itemQuants = ( typeof itemQuants === 'string' ) ? [itemQuants] : itemQuants;
+		console.log('itemnames is a ', typeof itemNames)
 	// `create_order params are are follows[ name, cost, status, phone_number, estimated_time ] 
 	Order.create_order( req.body.customerName, req.body.cost, 'pending', req.body.customerPhone, null )
 		.then( rows => {
+			orderID = rows[0];
 			// console.log('items: ', items)
 			itemNames.forEach(function( str, i, itemNames ) {
 				console.log('inside forEach, itemQuants is', itemQuants);
 				Item.find_by_name(str)
 					.then( item => {
-						// console.log('item.id = ', item.id, typeof item.id);
-						// console.log('id = ', rows[0], typeof rows[0] );
-						// console.log('item quants = ', itemQuants[i], typeof itemQuants[i]);
-						// console.log('item_price = ', item.item_price, typeof item.item_price);
-						OrderItem.create_orderitems(rows[0], Number(item.id), Number(itemQuants[i]), Number(item.item_price) )
+						OrderItem.create_orderitems(orderID, Number(item.id), Number(itemQuants[i]), Number(item.item_price) )
 							.then( () => {})
 							.catch( (err) => console.log('error creating orderitems', err))
 					})
@@ -49,15 +42,15 @@ router.post('/', (req, res) => {
 			});
 			console.log('Order items saved');
 		})
-		.then( id => {
+		.then( () => {
 
-		  console.log('[^ order # ' + id + ' created ]');
+		  console.log('[^ order # ' + orderID + ' created ]');
 			client.calls.create({
-		  	url: 'https://foodfast.fwd.wf/ivr/greeting/' + id ,
+		  	url: 'https://foodfast.fwd.wf/ivr/greeting/' + orderID ,
 		  	to: process.env.TEST_NUMBER,
 		  	from: process.env.TWILIO_NUMBER,
 			})
-			res.status(201).json({"message": 'Order' + id + 'created'});
+			res.status(201).json({"message": 'Order' + orderID + 'created'});
 		}).catch( (err) => {
 			res.status(500).json({error: 'Oops, order was not accepted.'});
 		})

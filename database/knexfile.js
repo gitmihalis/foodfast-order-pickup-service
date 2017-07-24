ed6c6d882d45919000e37961de088c9a3a6a645c
egomatsushita <eduardo.go.rj@gmail.com>
2017-07-24 02:39:13 +0000

    Clean up database folder

--- database/knexfile.js
+++ database/knexfile.js
@@ -2,7 +2,6 @@
 const settings = require("./settings");
 
 module.exports = {
-
   development: {
     client: 'pg',
     connection: {
@@ -11,38 +10,5 @@ module.exports = {
       password: settings.password,
       database: settings.database
     }
-  }/*,
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }*/
-
+  }
 };
